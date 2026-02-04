8134a9f0657de6ec212b851789a24c45824bf3e5
Jukaro <jeremi1005@outlook.fr>
2026-02-04 08:59:22 +0100

    refactor(audio-player): :recycle: refactor AudioPlayer

--- src/Core/Audio/AudioPlayer.cs
+++ src/Core/Audio/AudioPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,26 +20,28 @@ public class AudioPlayer {
 	private string _songPath;
 	private bool _needToPlayAudio;
 
-	private MiniAudioEngine _engine = new();
-	private FFmpegCodecFactory _ffmpegCodecFactory = new();
+	private static MiniAudioEngine _engine = new();
+	private static FFmpegCodecFactory _ffmpegCodecFactory = new();
 	private AudioFormat _audioFormat;
 	private StreamDataProvider? _streamDataProvider;
 	private AudioPlaybackDevice _playbackDevice;
 	private SoundPlayer _player;
 
-	public AudioPlayer(string songPath) {
-		_engine.RegisterCodecFactory(_ffmpegCodecFactory);
-
+	static AudioPlayer() {
 		// unregister default codec factory because it's too strict with audio files headers
 		_engine.UnregisterCodecFactory("SoundFlow.MiniAudio.Default");
 
+		_engine.RegisterCodecFactory(_ffmpegCodecFactory);
 		_engine.UpdateAudioDevicesInfo();
+	}
+
+	public AudioPlayer(string songPath) {
 		var defaultDevice = _engine.PlaybackDevices.FirstOrDefault(x => x.IsDefault);
 
 		_songPath = songPath;
-		Task.Run(SetAudioFormat).Wait();
+		Task.Run(() => _audioFormat = GetAudioFormat(songPath)).Wait();
 		_playbackDevice = _engine.InitializePlaybackDevice(defaultDevice, _audioFormat);
-		Task.Run(SetStreamDataProvider).Wait();
+		Task.Run(() => _streamDataProvider = new StreamDataProvider(_engine, File.OpenRead(_songPath))).Wait();
 
 		_player = new SoundPlayer(_engine, _audioFormat, _streamDataProvider);
 		_playbackDevice.MasterMixer.AddComponent(_player);
@@ -48,19 +51,17 @@ public class AudioPlayer {
 		_player.Volume = 0.5f;
 	}
 
-	private void SetAudioFormat() {
-		using var stream = File.OpenRead(_songPath);
+	private AudioFormat GetAudioFormat(string songPath) {
+		using var stream = File.OpenRead(songPath);
 		AudioFormat? format = AudioFormat.GetFormatFromStream(stream);
-		
-		if (format != null) {
-			_audioFormat = (AudioFormat)format;
-			return;
-		}
 
-		SetAudioFormatManually();
+		if (format != null)
+			return (AudioFormat)format;
+
+		return GetAudioFormatManually();
 	}
 
-	private void SetAudioFormatManually() {
+	private AudioFormat GetAudioFormatManually() {
 		var readOptions = new ReadOptions();
 		readOptions.ReadTags = false;
 
@@ -77,28 +78,25 @@ public class AudioPlayer {
 		if (!result.IsSuccess) {
 			string directory = Path.GetFileName(Path.GetDirectoryName(_songPath));
 			string filename = Path.GetFileName(_songPath);
-			Logger.LogError($"Couldn't read {Path.Combine(directory, filename)}: {result.Error.Message}");
-			return;
+			throw new ArgumentException($"Couldn't read {Path.Combine(directory, filename)}: {result.Error.Message}");
 		}
 
 		SoundFormatInfo info = result.Value;
 
-		_audioFormat = new AudioFormat();
-		_audioFormat.Format = intToSampleFormat[info.BitsPerSample];
-		_audioFormat.Channels = info.ChannelCount;
-		_audioFormat.SampleRate = info.SampleRate;
-		_audioFormat.Layout = AudioFormat.GetLayoutFromChannels(info.ChannelCount);
-	}
+		var audioFormat = new AudioFormat();
+		audioFormat.Format = intToSampleFormat[info.BitsPerSample];
+		audioFormat.Channels = info.ChannelCount;
+		audioFormat.SampleRate = info.SampleRate;
+		audioFormat.Layout = AudioFormat.GetLayoutFromChannels(info.ChannelCount);
 
-	private void SetStreamDataProvider() {
-		_streamDataProvider = new StreamDataProvider(_engine, File.OpenRead(_songPath));
+		return audioFormat;
 	}
 
 	public void Dispose() {
 		_playbackDevice.Stop();
 		_playbackDevice.Dispose();
 		_streamDataProvider?.Dispose();
-		_engine.Dispose();
+		_player.Dispose();
 	}
 
 	public void Pause() {
